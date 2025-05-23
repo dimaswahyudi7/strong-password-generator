4ce26f7e532eaab638b813264d001015dbb482f3
Dimas Wahyudi <55178812+dimaswahyudi7@users.noreply.github.com>
2025-05-23 13:50:34 +0700

    Delete password_generator.ps1

--- password_generator.ps1
+++
@@ -1,9 +0,0 @@
-function Generate-Password {
-    $lengthPrefix = 10
-    $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*'
-    $password = -join ((1..$lengthPrefix) | ForEach-Object { $chars[(Get-Random -Maximum $chars.Length)] })
-    $password += 'x' * 6
-    return $password
-}
-
-Write-Host "Generated Password: $(Generate-Password)"
